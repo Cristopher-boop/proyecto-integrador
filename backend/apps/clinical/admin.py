92e487de9cafda425fad1058782f73a3cd7f6bd0
Cristopher Diego Rodriguez Ayala <cristopherrodriguez1510@gmail.com>
2026-04-07 15:39:16 -0400

    chore: configuración del panel superadmin de Django

--- backend/apps/clinical/admin.py
+++ backend/apps/clinical/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CatComorbilidad, CatDiagnostico, ArchivoFuente, ObservacionBiomedica, MedicamentoAdmision, ComorbilidadAdmision, DiagnosticoEpisodio
 
-# Register your models here.
+admin.site.register(CatComorbilidad)
+admin.site.register(CatDiagnostico)
+admin.site.register(ArchivoFuente)
+admin.site.register(ObservacionBiomedica)
+admin.site.register(MedicamentoAdmision)
+admin.site.register(ComorbilidadAdmision)
+admin.site.register(DiagnosticoEpisodio)
